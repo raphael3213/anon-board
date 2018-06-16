4507455cd8eb8763b81c55def4ee2173b960bf09
Glitch (bitter-enquiry) <none>
2018-06-16 19:59:18 +0000

    🎳💵 Checkpoint ./public/client.js:1174538/396

--- public/client.js
+++ public/client.js
@@ -8,6 +8,25 @@
     url:'/all',
     datatype:'json',
     success: function(data){
+   
+      data.forEach(function(ele)
+                   {
+        var str="";
+        var k=0;      
+      ele.replies.forEach(function(rep,ind){
+      
+      k=ind+1;
+        str="<p>"+ind+")"+rep.comment+"<p>"
+      
+      })
+      
+      }
+                  
+                  
+                  )
+    
+    
+    
     
     console.log(data);
     
