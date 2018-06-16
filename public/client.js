f8b1770d643fdd48b7129a15b139a8f3094cd4f6
Glitch (bitter-enquiry) <none>
2018-06-16 21:02:06 +0000

    🍷💈 Checkpoint ./public/client.js:1174538/167

--- public/client.js
+++ public/client.js
@@ -23,19 +23,20 @@
       
       k=ind+1;
         str="<p>"+k+")"+rep.comment+"<p>";
-        strx="<input type='hidden' name=rname value="+rep.comment+">";
-        stry="<input type='submit'>"
-var str1="<form action='delrep' method='get'>"+str+"<input name='tid' type='hidden' value="+ele.id+">"+strx+"<label for='reppassword>Password:</label>'"+ "<input name='reppassword' type='text'>"+stry+"</form>";
+        strx="<input type='hidden' name='rname' value="+rep.comment+">";
+        stry="<input type='submit' value='delete'>"
+var str1="<form action='delrep' method='get'>"+str+"<input name='tid' type='hidden' value="+ele.id+">"+strx+"<label for='reppassword'>Password:</label>'"+ "<input name='reppassword' type='text'>"+stry+"</form>";
 
         arr.push(str1);
       
       })
         mstr=arr.join('');
-        var sdstr="<form action='addrep' method='post'><textarea name='reply'"
+        var fstr="<input type='hidden' name='tid' value="+ele.id+">";
+        var sdstr="<form action='addrep' method='post'><textarea name='reply' placeholder='reply....'></textarea><input type='submit'></form>'"
         var arrx=[ "<div id='booker'>",
            "<h3>Id:",ele.id,"</h3>",
            "<h3>Thread:",
-           ele.content,"</h3>",
+           ele.content,"</h3>",sdstr,
            "<h3>Replies:",
            "</h3>",mstr,
             "</div>"
