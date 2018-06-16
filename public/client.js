f07ceed5efa02cc907062b73d7e0829fcd11bd60
Glitch (bitter-enquiry) <none>
2018-06-16 20:40:55 +0000

    🌕📚 Checkpoint ./public/client.js:1174538/685

--- public/client.js
+++ public/client.js
@@ -12,16 +12,20 @@
       data.forEach(function(ele)
                    {
         var str="";
-        var str1="<form action='delrep' method='get'><input name='tid' type='hidden' value="+ele.id+"><label for='reppassword>Password:</label>'"+ "<input name='reppassword' type='text'>"+"</form>";
+        var strx="";
+        var stry="";
         var arr=[];
         arr.push("<div id='repl'>")
         var k=0;      
       ele.replies.forEach(function(rep,ind){
       
       k=ind+1;
-        str="<p>"+ind+")"+rep.comment+"<p>";
-        
-        
+        str="<p>"+k+")"+rep.comment+"<p>";
+        strx="<input type='hidden' name=rname value="+rep.comment+">";
+        stry="<input type='submit'>"
+var str1="<form action='delrep' method='get'>"+str+"<input name='tid' type='hidden' value="+ele.id+">"+strx+"<label for='reppassword>Password:</label>'"+ "<input name='reppassword' type='text'>"+stry+"</form>";
+
+        arr.push(str1);
       
       })
       
