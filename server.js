282f908bfb21ef655b74b802e6e8c039c7c848da
Glitch (bitter-enquiry) <none>
2018-06-16 14:01:53 +0000

    🎐🎻 Checkpoint ./server.js:1174538/213 ./views/index.html:1174538/606

--- server.js
+++ server.js
@@ -4,12 +4,24 @@
 // init project
 var express = require('express');
 var app = express();
+var mongoose=require('mongoose');
+var bp=require('body-parser');
 
+
+mongoose.connect(process.env.SECRET,function(err){
+
+if(err){console.log(err)}
+  
+  console.log("mongoose connected")
+
+})
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
